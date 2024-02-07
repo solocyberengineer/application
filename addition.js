f8cfc359f0338b23c0cd466517a598585beff1fa
solocyberengineer <solocyberengineer@gmail.com>
2024-02-07 10:07:30 +0200

    updated addition and subtraction

--- addition.js
+++ addition.js
@@ -3,3 +3,7 @@ console.log('addition')
 function add(...args){
     return args.reduce((a,b)=>{ return a+b },0)
 }
+
+module.exports = {
+    add
+}
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const { add } = require('./addition');
+const { sub } = require('./subtraction');
 
 
+let x = add(1,2,3,4)
+console.log(x)
+let y = sub(10, 2)
+console.log(y)
